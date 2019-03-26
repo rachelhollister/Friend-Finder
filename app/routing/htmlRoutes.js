8f556e1534740ada78ea43fa67f07f3ab41c0c9c
Rachel Hollister <rachelhollister@Rachels-MacBook-Pro.local>
2019-03-25 23:27:45 -0700

    updated README.MD

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,10 +2,10 @@ var path = require("path");
 
 // routing paths to be applied to buttons
 module.exports = function(app) {
-    app.get("/survey", function(req.res){
+    app.get("/survey", function(req, res){
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
-    app.use(function(req.res){
+    app.use(function(req, res){
         res.sendFile(path.join(__dirname + "/..public/home.html"));
     });
 }
--- server.js
+++ server.js
@@ -10,13 +10,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use(express.static(app/public)); //not sure what this does? for publishing?
+app.use(express.static("app/public")); //not sure what this does? for publishing?
 
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+// app.listen(port, () => console.log("Listening on port %s", port));
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
