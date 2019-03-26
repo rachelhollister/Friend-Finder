a46861b644ba6285e2f604af96ceaaec49edda9c
Rachel Hollister <rachelhollister@Rachels-MacBook-Pro.local>
2019-03-26 01:57:54 -0700

    updated README.MD

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,23 @@
+// dependencies
+// We need to include the path package to get the correct file path for our html
 var path = require("path");
 
-// routing paths to be applied to buttons
+//routing
+//paths to be applied to buttons
 module.exports = function(app) {
+// HTML GET Requests
+// Below code handles when users "visit" a page.
+// In each of the below cases the user is shown an HTML page of content
+
     app.get("/survey", function(req, res){
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
     app.use(function(req, res){
-        res.sendFile(path.join(__dirname + "/..public/home.html"));
+        res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
-}
+
+    // If no matching route is found default to home.html
+    app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+};
--- server.js
+++ server.js
@@ -1,49 +1,79 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
-
-var app = express();
-var PORT = process.env.PORT || 8080;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-app.use(express.static("app/public")); //not sure what this does? for publishing?
-
-
-
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
-
-
-
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
-
 // var express = require("express");
 // var bodyParser = require("body-parser");
 // var path = require("path");
 
 // var app = express();
-// var port = process.env.PORT || 8080;
+// var PORT = process.env.PORT || 8080;
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.text());
 // app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+// app.use(express.static("app/public")); //not sure what this does? for publishing?
 
-// app.use(express.static("app/public"));
 
-// require("./app/routing/apiRoutes")(app);
-// require("./app/routing/htmlRoutes")(app);
 
-// app.listen(port, () => console.log("Listening on port %s", port));
+// require("./app/routing/apiRoutes.js")(app);
+// require("./app/routing/htmlRoutes.js")(app);
+
+
+
+// app.listen(PORT, function() {
+//   console.log("App listening on PORT: " + PORT);
+// });
+
+
+
+
+
+
+
+
+
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
+
 
 
