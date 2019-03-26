14c7b5bca57defe0347f7e27d0592ffa5027a48f
Rachel Hollister <rachelhollister@Rachels-MacBook-Pro.local>
2019-03-26 01:21:28 -0700

    updated

--- server.js
+++ server.js
@@ -1,47 +1,49 @@
-// var express = require("express");
-// var bodyParser = require("body-parser");
-// var path = require("path");
-
-// var app = express();
-// var PORT = process.env.PORT || 8080;
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-// app.use(express.static("app/public")); //not sure what this does? for publishing?
-
-
-
-// require("./app/routing/apiRoutes.js")(app);
-// require("./app/routing/htmlRoutes.js")(app);
-
-
-
-// app.listen(PORT, function() {
-//   console.log("App listening on PORT: " + PORT);
-// });
-
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var port = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use(express.static("app/public"));
+app.use(express.static("app/public")); //not sure what this does? for publishing?
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
 
-app.listen(port, () => console.log("Listening on port %s", port));
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
+// var express = require("express");
+// var bodyParser = require("body-parser");
+// var path = require("path");
+
+// var app = express();
+// var port = process.env.PORT || 8080;
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+// app.use(express.static("app/public"));
+
+// require("./app/routing/apiRoutes")(app);
+// require("./app/routing/htmlRoutes")(app);
+
+// app.listen(port, () => console.log("Listening on port %s", port));
+
 
